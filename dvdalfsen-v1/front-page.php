194063d34f84dfdb013d9a36ce51f844b0918df9
dvdalfsen <70368741+dvdalfsen@users.noreply.github.com>
2025-01-10 12:54:23 +0100

    Add files via upload

---
+++ dvdalfsen-v1/front-page.php
@@ -0,0 +1,19 @@
+<?php 
+
+get_header(); 
+
+?>
+
+<main id="frontpage">
+    <?php
+   
+    get_template_part('page-templates/home');
+    ?>
+	
+</main>
+
+<?php 
+
+get_footer(); 
+
+?>
---
+++ dvdalfsen-v1/index.php
@@ -0,0 +1,21 @@
+<?php 
+
+get_header(); 
+
+?>
+
+<main>
+
+    <?php
+    
+    get_template_part('partials/base');
+
+    ?>
+	
+</main>
+
+<?php 
+
+get_footer(); 
+
+?>
