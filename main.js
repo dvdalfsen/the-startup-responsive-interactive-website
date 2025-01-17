3b28c641c8d6f08eb0d96ab638459613afd022fb
dvdalfsen <70368741+dvdalfsen@users.noreply.github.com>
2025-01-17 12:39:46 +0100

    changes

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
   
   var slider = tns({
     container: '.my-slider',
-    items: 1,
+    items: 2,
     controls: false,
     nav: false,
     // autoplay: true,
     // autoplayButton: false,
-    // edgePadding: 50,
+    edgePadding: 50,
+    // gutter: 30,
     center: true,
+    touch: true,
+    mouseDrag: true,
   });
