40c87ab8ac3a66b34a60bf505674bb0b1af77296
dvdalfsen <70368741+dvdalfsen@users.noreply.github.com>
2025-01-20 10:29:05 +0100

    changes mobile first

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
   
   var slider = tns({
     container: '.my-slider',
-    items: 2,
+    items: 1,
     controls: false,
     nav: false,
     // autoplay: true,
     // autoplayButton: false,
-    edgePadding: 50,
-    // gutter: 30,
+    // edgePadding: 50,
     center: true,
-    touch: true,
-    mouseDrag: true,
+  });
+
+  var slider = tns({
+    container: '.my-slider-mobile',
+    items: 3,
+    controls: false,
+    nav: false,
+    // axis: vertical,
+    // autoplay: true,
+    // autoplayButton: false,
+    // edgePadding: 50,
+    center: true,
   });
