84a51478bc3fe77657f98066bbaed82527335831
dvdalfsen <70368741+dvdalfsen@users.noreply.github.com>
2025-01-20 11:10:15 +0100

    disable dots slider

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
   var elem = document.querySelector('.main-carousel');
   var flkty = new Flickity( elem, {
     // options
-    cellAlign: 'center',
-    // contain: true,
+    cellAlign: 'center',  
     prevNextButtons: false,
-    draggable: true,
-    wrapAround: true,
-    autoPlay: true,
+    pageDots: false,
   });
   
   // element argument can be a selector string
